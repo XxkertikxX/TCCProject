8ff617d051521f84bb12bd7a58282f0dc9e4cb0d
XxkertikxX <nichonabity@gmail.com>
2025-05-29 09:54:54 -0300

    Add Movement Player Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float x;
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        rb.linearVelocity = new Vector2(x, rb.linearVelocity.y).normalized * speed;
+    }
+
+    void Update() {
+        InputDirectionMovement();
+        Jump();
+    }
+
+    void InputDirectionMovement() {
+        x = Input.GetAxisRaw("Horizontal");
+    }
+
+    void Jump(){
+        if(Input.GetButtonDown("Jump")) {
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+}
