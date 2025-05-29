548c1acccbdde8e011cbcd0010adfa964ece97b3
XxkertikxX <nichonabity@gmail.com>
2025-05-29 09:53:13 -0300

    Add Script Change Scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] string NextScene;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("troca")) {
+            SceneManager.LoadScene(NextScene);
+        }
+    }
+}
