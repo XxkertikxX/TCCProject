0567cba66214df9255613dadb79dd9a285bb8bd6
XxkertikxX <nichonabity@gmail.com>
2025-05-29 12:37:51 -0300

    Update FollowPlayer

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] Transform Target;
+    Vector3 pos;
+    [SerializeField] int Position;
+    public bool CanFollowY;
     void Update() {
-        transform.position = Target.position;
+        FollowX();
+        if (CanFollowY) {
+            pos.y = Mathf.Lerp(pos.y, Target.position.y, Time.deltaTime*5);
+        }
+        transform.position = pos;
+    }
+
+    void FollowX() {
+        pos.x = Target.position.x;
+    }
+
+    public async void FollowY() {
+        await Task.Delay(150 * Position);
+        CanFollowY = true;
     }
 }
