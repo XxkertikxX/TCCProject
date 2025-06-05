339efe11070afa9dcee3096e5b836b128b460211
XxkertikxX <nichonabity@gmail.com>
2025-06-05 11:51:39 -0300

    Refactoring CreateBindingDB

--- Assets/Scripts/CreateBindingDB.cs
+++ Assets/Scripts/CreateBindingDB.cs
@@ -3,18 +3,26 @@ using LiteDB;
 
 public class CreateBindingDB : MonoBehaviour
 {
-    void Start(){
-        var path = Application.persistentDataPath + "/bindings.db";
-
-        if (System.IO.File.Exists(path)) {
-            System.IO.File.Delete(path);
-        }
-
-        using (var db = new LiteDatabase(path)) {
-                
+    void Start() {
+        using (var db = new LiteDatabase(Path())) {
+            DestroyExistentDB();
             var col = db.GetCollection<KeyBinding>("bindings");
-
-            col.Upsert(new KeyBinding { keyName = "Jump", key = KeyCode.Space });
+            CreateKeys(col);
         }
+    }
+    void DestroyExistentDB(){
+        if (System.IO.File.Exists(Path())) {
+            System.IO.File.Delete(Path());
+        }
+    }
+
+    string Path() {
+        return Application.persistentDataPath + "/bindings.db";
+    }
+
+    void CreateKeys(ILiteCollection<KeyBinding> col){
+        col.Upsert(new KeyBinding { keyName = "Jump", key = KeyCode.Space });
+        col.Upsert(new KeyBinding { keyName = "Left", key = KeyCode.A });
+        col.Upsert(new KeyBinding { keyName = "Right", key = KeyCode.D });
     }
 }
