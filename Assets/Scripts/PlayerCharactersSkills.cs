f6ea313581d74dbadd455bf3ca8999410011fef1
XxkertikxX <nichonabity@gmail.com>
2025-06-10 18:29:21 -0300

    Include Interface

--- Assets/Scripts/PlayerCharactersSkills.cs
+++ Assets/Scripts/PlayerCharactersSkills.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class PlayerCharactersSkills : MonoBehaviour
 {
     public void PressButtonSkill1(ISkills skill) {
-        skill.Skill1();
+        skill.Skill1(new StatusCharacters()); //Temp
     }
 
     public void PressButtonSkill2(ISkills skill) {
-        skill.Skill2();
+        skill.Skill2(new StatusCharacters());
+    }
+
+    ISkills character() {
+        return null;
     }
 }
