5694c307743098abac5f8cd63d37567168713076
XxkertikxX <nichonabity@gmail.com>
2025-06-13 22:37:40 -0300

    Remove parts removed from the interface

--- Assets/Scripts/PlayerCharactersSkills.cs
+++ Assets/Scripts/PlayerCharactersSkills.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class PlayerCharactersSkills : MonoBehaviour
 {
-    public void PressButtonSkill1(ISkills skill) {
-        skill.Skill1(new StatusCharacters()); //Temp
-    }
-
-    public void PressButtonSkill2(ISkills skill) {
-        skill.Skill2(new StatusCharacters());
-    }
-
-    ISkills character() {
-        return null;
+    public void PressButtonSkill(ISkill skill) {
+        skill.Skill(new StatusCharacters()); //Temp
     }
 }
