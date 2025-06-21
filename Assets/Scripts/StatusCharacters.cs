03ac1f93cd2c84e22ae1b9c5076a60f40153eea4
XxkertikxX <nichonabity@gmail.com>
2025-06-21 17:57:28 -0300

    Add Skills

--- Assets/Scripts/StatusCharacters.cs
+++ Assets/Scripts/StatusCharacters.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-[CreateAssetMenu(menuName ="CharacterCreator")]
+[CreateAssetMenu(menuName = "CharacterCreator")]
 public class StatusCharacters : ScriptableObject
 {
     public int hp;
     public int damage;
     public bool AttackInTheTurn;
+    public List<ISkill> skills;
 }
