9666c3382b2a6c57272bd6ac4c3988e757a280f9
XxkertikxX <nichonabity@gmail.com>
2025-06-22 13:39:51 -0300

    Add SystemRhythm Script

---
+++ Assets/Scripts/SystemRhythm.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class SystemRhythm : MonoBehaviour
+{
+    [SerializeField] GameObject Line;
+    [SerializeField] Transform InstantiatePosition;
+    [SerializeField] int TimesForInvoke;
+    [SerializeField] float TimePerInvokeLine;
+
+    void OnEnable() {
+        StartCoroutine(SpawnLines());
+    }
+
+    IEnumerator SpawnLines() {
+        while (TimesForInvoke > 0) {
+            Instantiate(Line, InstantiatePosition.position, Quaternion.identity);
+            TimesForInvoke--;
+            yield return new WaitForSeconds(TimePerInvokeLine);
+        }
+    }
+}
