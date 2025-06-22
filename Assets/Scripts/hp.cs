ef63ab5fed0080395b056d5a396c9a844625783a
XxkertikxX <nichonabity@gmail.com>
2025-06-22 12:47:48 -0300

    Add Death To Characters

--- Assets/Scripts/hp.cs
+++ Assets/Scripts/hp.cs
@@ -1,12 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class hp : MonoBehaviour
 {
-    float Life;
+    StatusCharacters Character;
+    float life;
+
+    void Start() {
+        Character = GetComponent<CharacterStatus>().character;
+        life = Character.hp;
+    }
 
     public void TakeDamage(float Damage) {
-        Life -= Damage;
+        life -= Damage;
+        if (IsDead()) {
+            Destroy(gameObject);
+        }
+    }
+
+    bool IsDead() {
+        if (life <= 0) {
+            return true;
+        }
+        return false;
     }
 }
