3bbf781caecafc0c52a800a2cae6b3a9bb77f546
XxkertikxX <nichonabity@gmail.com>
2025-06-22 12:47:36 -0300

    Add EnemyTurn Script

---
+++ Assets/Scripts/EnemyTurn.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurn : MonoBehaviour
+{
+    [SerializeField] StatusCharacters Enemy;
+
+    void Update() {
+        if (AllCharactersPlay()) {
+            EnemyAttack();
+        }
+    }
+    void EnemyAttack() {
+        int randomSkill = Random.Range(0, Enemy.skills.Count);
+        Enemy.skills[randomSkill].Skill();
+    }
+
+    bool AllCharactersPlay() {
+        var characters = GameObject.FindGameObjectsWithTag("Character");
+        foreach (var character in characters) {
+            if (character.GetComponent<CharacterStatus>().character.AttackInTheTurn) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
