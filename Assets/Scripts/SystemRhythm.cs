e8aba6bbe7b696e5f73e7358775e8be1aced6044
XxkertikxX <nichonabity@gmail.com>
2025-06-24 20:55:40 -0300

    Damage After Rhythm

--- Assets/Scripts/SystemRhythm.cs
+++ Assets/Scripts/SystemRhythm.cs
@@ -3,20 +3,37 @@ using UnityEngine;
 
 public class SystemRhythm : MonoBehaviour
 {
+    static public int PosSkill;
     [SerializeField] GameObject Line;
     [SerializeField] Transform InstantiatePosition;
-    [SerializeField] int TimesForInvoke;
-    [SerializeField] float TimePerInvokeLine;
-    
+
     void OnEnable() {
+        CatalystSkills.Damage = 0;
         StartCoroutine(SpawnLines());
     }
 
     IEnumerator SpawnLines() {
+        SkillBase skill = PlayerCharactersSkills.character.skills[PosSkill];
+        int TimesForInvoke = skill.TimesForInvoke;
         while (TimesForInvoke > 0) {
-            Instantiate(Line, InstantiatePosition.position, Quaternion.identity);
+            var line = Instantiate(Line, InstantiatePosition.position, Quaternion.identity);
+            line.GetComponent<LineRhythm>().linesMissingSpawn = TimesForInvoke;
             TimesForInvoke--;
-            yield return new WaitForSeconds(TimePerInvokeLine);
+            yield return new WaitForSeconds(skill.TimePerInvokeLine);
         }
+    }
+
+    void OnDisable() {
+        UseSkill();
+    }
+    
+    void UseSkill() {
+        var character = PlayerCharactersSkills.character;
+        character.skills[PosSkill].Skill(character.damage, Enemyhp());
+        character.AttackInTheTurn = true;
+    }
+
+    hp Enemyhp() {
+        return GameObject.FindGameObjectWithTag("Enemy").GetComponent<hp>();
     }
 }
