a0dd4532a856cb7f00398b520486a84fe51d690f
XxkertikxX <nichonabity@gmail.com>
2025-06-24 20:53:29 -0300

    Add TimePerInvokeLine Var

--- Assets/Scripts/SkillBase.cs
+++ Assets/Scripts/SkillBase.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public abstract class SkillBase : ScriptableObject
 {
     public int TimesForInvoke;
-
+    public float TimePerInvokeLine;
+    
     public abstract void Skill(float power, hp target);
 }
