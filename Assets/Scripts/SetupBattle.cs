ce666630ce7d7e441e2f6a0d921138df76e7d6c2
XxkertikxX <nichonabity@gmail.com>
2025-06-26 10:44:15 -0300

    Add Disable Buttons Click

--- Assets/Scripts/SetupBattle.cs
+++ Assets/Scripts/SetupBattle.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetupBattle : MonoBehaviour
 {
@@ -8,9 +9,18 @@ public class SetupBattle : MonoBehaviour
 
     void Start(){
         dial.IniciarDialogo(dialogo.linhas);
+        StatesUIButton(false);
     }
-
+    
     public void ActiveUI(){
         UI.SetActive(true);
+        StatesUIButton(true);
+    }
+
+    void StatesUIButton(bool state){
+        Button[] botoes = FindObjectsOfType<Button>();
+        foreach (Button b in botoes) {
+            b.interactable = state;
+        }
     }
 }
