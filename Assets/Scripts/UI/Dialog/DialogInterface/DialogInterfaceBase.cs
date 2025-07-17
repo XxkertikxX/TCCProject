f9a60eb8be69a7f464d0f4d0e1e1205522517f6d
XxkertikxX <nichonabity@gmail.com>
2025-07-17 16:12:28 -0300

    Add DialogInterfaceBase Script

---
+++ Assets/Scripts/UI/Dialog/DialogInterface/DialogInterfaceBase.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogInterfaceBase : MonoBehaviour
+{
+    [SerializeField] protected Text textSpeak;
+    protected LineDialog[] dialogs;
+    protected int index;
+
+    void OnEnable() {
+        DialogManager.OnDialogOpen += Setup;
+    }
+
+    void OnDisable() {
+        DialogManager.OnDialogOpen -= Setup;
+    }
+    
+    private void Setup(){
+        index = 0;
+        dialogs = DialogManager.InstanceDialogManager.dialogs;
+    }
+}
