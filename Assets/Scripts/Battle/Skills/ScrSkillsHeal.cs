50802a4ac00a995f73c93d646f9862744992a809
XxkertikxX <nichonabity@gmail.com>
2025-07-18 13:52:29 -0300

    Add Heal Mechanics

--- Assets/Scripts/Battle/Skills/ScrSkillsHeal.cs
+++ Assets/Scripts/Battle/Skills/ScrSkillsHeal.cs
@@ -6,6 +6,8 @@ public class ScrSkillsHeal : SkillBase
     public override void Skill(float power) {
         float heal = power * (CatalystSkills.Damage / TimesForInvoke);
         float roundHeal = Mathf.Round(heal);
-        TargetType.targets()[0].Hp.Heal(roundHeal);
+        foreach (var target in TargetType.targets()) {
+            target.Hp.AddLife(roundHeal);
+        }
     }
 }
