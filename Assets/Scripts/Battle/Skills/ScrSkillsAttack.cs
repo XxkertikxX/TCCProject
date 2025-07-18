8dbfec0cae938f561d4c59d74797b8ba9fffa4af
XxkertikxX <nichonabity@gmail.com>
2025-07-18 13:52:19 -0300

    Add Attack Mechanics

--- Assets/Scripts/Battle/Skills/ScrSkillsAttack.cs
+++ Assets/Scripts/Battle/Skills/ScrSkillsAttack.cs
@@ -5,6 +5,9 @@ public class ScrSkillsAttack : SkillBase
 {
     public override void Skill(float power) {
         float damage = power * (CatalystSkills.Damage / TimesForInvoke);
-        float RoundDamage = Mathf.Round(damage);
+        float RoundDamage = -Mathf.Round(damage);
+        foreach (var target in TargetType.targets()) {
+            target.Hp.AddLife(RoundDamage);
+        }
     }
 }
