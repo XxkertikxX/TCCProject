111610915369a4c7e2b65f6797180f2b73745025
XxkertikxX <nichonabity@gmail.com>
2025-07-28 21:37:06 -0300

    Refactoring PlayerMovementWalk

---
+++ Assets/Scripts/MoveSystem/Movements/PlayerMovementWalk.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovementWalk : MonoBehaviour, IMovement
+{    
+    private float speed = 5f;
+    private float inputDirection;
+
+    public void Move(Rigidbody2D rb) {
+        rb.velocity = new Vector2(inputDirection * speed, rb.velocity.y);
+    }
+
+    public bool Apply(IButtonInput input) {
+        InputDirectionCalculator(input);
+        return true;
+    }
+
+    private void InputDirectionCalculator(IButtonInput input) {
+        inputDirection = 0;
+        inputDirection -= GetDirectionX(input, "Left");
+        inputDirection += GetDirectionX(input, "Right");
+    }
+
+    private float GetDirectionX(IButtonInput input, string inputKey) {
+        return input.InputButton(inputKey) ? 1f : 0f;
+    }
+}
