b451c096650704b676ad814c6bf0c5743a1be46a
XxkertikxX <nichonabity@gmail.com>
2025-07-28 21:36:52 -0300

    Refactoring PlayerMovementJump

---
+++ Assets/Scripts/MoveSystem/Movements/PlayerMovementJump.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMovementJump : MonoBehaviour, IMovement
+{        
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
+
+    private float jumpForce = 10f;
+
+    public void Move(Rigidbody2D rb) {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public bool Apply(IButtonInput input) {
+        bool keyPressed = input.InputButtonDown("Jump");
+        bool isGrounded  = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.05f, ground);
+        return isGrounded && keyPressed;
+    }
+}
