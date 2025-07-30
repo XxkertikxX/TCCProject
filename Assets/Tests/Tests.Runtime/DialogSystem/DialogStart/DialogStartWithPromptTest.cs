de3178752dcefd1db06ffb69cdc22042724a83f7
Nicholas Nabity <aluno@p0610.etecaf.local>
2025-07-30 16:04:16 -0300

    Add DialogStartWithPromptTest

--- Assets/Tests/Tests.Runtime/DialogSystem/DialogStart/DialogStartWithPromptTest.cs
+++ Assets/Tests/Tests.Runtime/DialogSystem/DialogStart/DialogStartWithPromptTest.cs
@@ -5,5 +5,29 @@ using UnityEngine.TestTools;
 
 public class DialogStartWithPromptTest : RuntimeTestBase
 {
+    private DialogStartWithPrompt dialogPrompt;
+    private GameObject prompt;
 
+    [SetUp]
+    public void Setup() {
+        CreatePlayer();
+        prompt = new GameObject("Prompt");
+        CreateDialogPrompt();
+    }
+
+    [UnityTest]
+
+
+    private void CreatePlayer() {
+        GameObject player = new GameObject("Player");
+        player.tag = "Player";
+        player.AddComponent<PlayerMovementSystem>();
+        player.AddComponent<BoxCollider2D>();
+    }
+
+    private void CreateDialogPrompt() {
+        GameObject dialogPromptObj = new GameObject("DialogPrompt");
+        dialogPrompt = dialogPromptObj.AddComponent<DialogStartWithPrompt>();
+        dialogPromptObj.AddComponent<BoxCollider2D>().isTrigger = true;
+    }
 }
