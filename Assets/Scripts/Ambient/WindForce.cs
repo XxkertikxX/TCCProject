7ccf9c1031bf94e423bce9c8640995a981d8f353
gaelExp <pedroandmelo2000@gmail.com>
2025-08-01 21:59:11 -0300

    Paranaues

--- Assets/Scripts/Ambient/WindForce.cs
+++ Assets/Scripts/Ambient/WindForce.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class WindForce : MonoBehaviour
 {
     float windSpeed = .5f;
-    public float windDistance = 10f;
     public Vector2 windDirection;
+    public float windForce;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision == null || collision.gameObject.tag != "Player")
         {
             return;
-        }
+        } Debug.Log("Esta no collider");
         Rigidbody2D playerRB = collision.gameObject.GetComponent<Rigidbody2D>();
-        Vector2 relativeForce = Vector2.Lerp(collision.transform.position - transform.position, transform.position, 0.1f);
-        playerRB.AddForce(windSpeed * relativeForce * windDirection, ForceMode2D.Force);
+        playerRB.AddForce(windDirection * windForce);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision == null || collision.gameObject.tag != "Player")
+        {
+            return;
+        }Debug.Log("Saiu do collider");
+        Rigidbody2D playerRB = collision.gameObject.GetComponent<Rigidbody2D>();
+        //playerRB.velocity = Vector2.zero;
     }
 }
