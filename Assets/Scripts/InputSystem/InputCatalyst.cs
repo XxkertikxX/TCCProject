76a1956ee01719d874e70e37d3bc180f2195fba3
Nicholas Nabity <nicholasbritonabity@gmail.com>
2025-08-07 14:31:05 -0300

    Remove Monobehaviour

--- Assets/Scripts/InputSystem/InputCatalyst.cs
+++ Assets/Scripts/InputSystem/InputCatalyst.cs
@@ -1,13 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(IButtonInput))]
-public class InputCatalyst : MonoBehaviour
-{
+public class InputCatalyst {
     static public IButtonInput input;
-
-    void Awake() {
-        input = GetComponent<IButtonInput>();
-    }
 }
