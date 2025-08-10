6cf0de879d7752b6a1ec9f70aa999eba65a61b3b
gaelExp <pedroandmelo2000@gmail.com>
2025-08-10 01:35:10 -0300

    Update LightningPlataformscr.cs

--- Assets/Scripts/Ambient/Raio/LightningPlataformscr.cs
+++ Assets/Scripts/Ambient/Raio/LightningPlataformscr.cs
@@ -7,10 +7,12 @@ public class LightningPlataformscr : MonoBehaviour, IEnviromentProperty
     [SerializeField] private Rigidbody2D platformRb;
     [SerializeField] private float delay = 3f;
     [SerializeField] private float targetGravity = 1f;
+    private ParticleSystem thunderParticle;
 
     private void Start()
     {
         platformRb.constraints = RigidbodyConstraints2D.FreezeAll;
+        thunderParticle = GetComponentInChildren<ParticleSystem>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,7 +26,14 @@ public class LightningPlataformscr : MonoBehaviour, IEnviromentProperty
     public IEnumerator ApplyEffect(Rigidbody2D targetRB)
     {
         yield return new WaitForSeconds(delay);
+        StartCoroutine(Particle());
         targetRB.gravityScale = targetGravity;
         targetRB.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+    }
+    IEnumerator Particle()
+    {
+        thunderParticle.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        thunderParticle.gameObject.SetActive(false);
     }
 }
