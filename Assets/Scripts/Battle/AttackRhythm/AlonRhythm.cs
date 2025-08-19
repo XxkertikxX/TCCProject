f5b760cb4c2289fb877f676ab029a60016b969a9
Nicholas Nabity <nichonabity@gmail.com>
2025-08-19 18:38:08 -0300

    Create AttackRhythm Scripts

---
+++ Assets/Scripts/Battle/AttackRhythm/AlonRhythm.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlonRhythm : AttackRhythm
+{
+    public IEnumerator Attack(SkillBase skill) {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Battle/AttackRhythm/LioraRhythm.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LioraRhythm : AttackRhythm
+{
+    public IEnumerator Attack(SkillBase skill) {
+        yield return null;
+    }
+}
