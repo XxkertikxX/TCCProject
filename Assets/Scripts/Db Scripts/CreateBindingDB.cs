e378f968f600c1f25d7292b956ed395fd6683c59
XxkertikxX <nichonabity@gmail.com>
2025-08-23 19:05:06 -0300

    Add leftShift key

--- Assets/Scripts/Db Scripts/CreateBindingDB.cs
+++ Assets/Scripts/Db Scripts/CreateBindingDB.cs
@@ -25,7 +25,7 @@ public class CreateBindingDB : MonoBehaviour
         CreateKey("Jump", KeyCode.Space, col);
         CreateKey("Left", KeyCode.A, col);
         CreateKey("Right", KeyCode.D, col);
-        CreateKey("Run", KeyCode.Shift, col);
+        CreateKey("Run", KeyCode.LeftShift, col);
         CreateKey("Interact", KeyCode.E, col);
         CreateKey("Skip", KeyCode.Return, col);
         CreateKey("Menu", KeyCode.Escape, col);
