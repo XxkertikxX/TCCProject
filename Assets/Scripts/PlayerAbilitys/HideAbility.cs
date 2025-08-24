efeccf4186e3ed3481092080a72f9efd3151bcf0
bryan270906 <bjesusdealbuquerque@gmail.com>
2025-08-24 15:42:55 -0300

    arrumei o bglh de se esconder

--- Assets/Scripts/PlayerAbilitys/HideAbility.cs
+++ Assets/Scripts/PlayerAbilitys/HideAbility.cs
@@ -1,21 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class HideAbility : MonoBehaviour
 {
+    private Coroutine fadeRoutine;
+
     private bool hide = false;
     public bool Hide { get { return hide; } set { hide = value; } }
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.CompareTag("HideWall")) {
-            hide = true;
-        }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("HideWall")) return;
+        hide = true;
+        if (fadeRoutine != null) StopCoroutine(fadeRoutine);
+        fadeRoutine = StartCoroutine(
+            FadeTo(collision.GetComponent<SpriteRenderer>(), 0.5f, 0.1f)
+        );
     }
 
-    private void OnTriggerExit2D(Collider2D collision) {
-        if (collision.gameObject.CompareTag("HideWall")) {
-            hide = false;
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("HideWall")) return;
+        hide = false;
+        if (fadeRoutine != null) StopCoroutine(fadeRoutine);
+        fadeRoutine = StartCoroutine(
+            FadeTo(collision.GetComponent<SpriteRenderer>(), 1f, 0.1f)
+        );
+    }
+    private IEnumerator FadeTo(SpriteRenderer sprite, float targetAlpha, float duration)
+    {
+        float startAlpha = sprite.color.a;
+        float time = 0f;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = time / duration;
+            Color c = sprite.color;
+            c.a = Mathf.Lerp(startAlpha, targetAlpha, t);
+            sprite.color = c;
+            yield return null;
         }
+        Color finalColor = sprite.color;
+        finalColor.a = targetAlpha;
+        sprite.color = finalColor;
     }
 }
