ff2c6abb66a72224b593435fcfcce25fdc9c2432
XxkertikxX <nichonabity@gmail.com>
2025-08-25 11:33:21 -0300

    Bryan...

---
+++ Assets/Scripts/Ambient/Sombras/Bat/BatMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BatMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Awake() => rb = GetComponent<Rigidbody2D>();
+
+    public void Move(Vector2 direction, float speed) {
+        rb.velocity = direction.normalized * speed;
+    }
+
+    public void Stop() => rb.velocity = Vector2.zero;
+}
---
+++ Assets/Scripts/Ambient/Sombras/Bat/IBatState.cs
@@ -0,0 +1,5 @@
+public interface IBatState {
+    public void Enter();
+    public void Update();
+    public void Exit();
+}
