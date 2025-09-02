654b09cfd2867d1a50f0a264fbc099148e1ea841
Nicholas Nabity <nichonabity@gmail.com>
2025-09-02 13:53:11 -0300

    Targets

--- Assets/Scripts/Battle/Skills/TypesSkills/TargetSelectCharacter.cs
+++ Assets/Scripts/Battle/Skills/TypesSkills/TargetSelectCharacter.cs
@@ -5,55 +5,7 @@ using System.Collections.Generic;
 [CreateAssetMenu(menuName = "TargetSelectCharacter")]
 public class TargetSelectCharacter : TypeSkill
 {
-    public GameObject SelectedCharacter;
-    public int SelectedIndex;
-    public float SelectionTimeout = -1f;
-
-    public bool IsSelecting { get; private set; }
-    public bool IsConfirmed { get; private set; }
-
-    public override IEnumerator Targets()
-    {
-        IsSelecting = true;
-        IsConfirmed = false;
-        float t = 0f;
-        while (!IsConfirmed)
-        {
-            if (SelectionTimeout > 0f)
-            {
-                t += Time.deltaTime;
-                if (t >= SelectionTimeout) break;
-            }
-            yield return null;
-        }
-        IsSelecting = false;
-        CharactersAttributes = BuildListFrom(GetSelected());
-        ClearSelected();
-        yield return null;
-    }
-
-    public void SetSelectedCharacter(GameObject go) { SelectedCharacter = go; }
-    public void SetSelectedIndex(int i) { SelectedIndex = i; }
-    public void ConfirmSelection() { IsConfirmed = true; }
-    public void CancelSelection() { IsConfirmed = false; IsSelecting = false; ClearSelected(); }
-    public void ClearSelected() { SelectedCharacter = null; SelectedIndex = 0; }
-
-    private GameObject[] GetAllChars() => GameObject.FindGameObjectsWithTag("Character");
-
-    private GameObject GetSelected()
-    {
-        if (SelectedCharacter != null) return SelectedCharacter;
-        var chars = GetAllChars();
-        if (chars.Length == 0) return null;
-        return chars[Mathf.Clamp(SelectedIndex, 0, chars.Length - 1)];
-    }
-
-    private List<CharacterAttributes> BuildListFrom(GameObject go)
-    {
-        var list = new List<CharacterAttributes>();
-        if (go == null) return list;
-        var a = go.GetComponent<CharacterAttributes>();
-        if (a != null) list.Add(a);
-        return list;
+    public override IEnumerator Targets() {
+        
     }
 }
--- Assets/Scripts/Battle/Skills/TypesSkills/targetThisCharacter.cs
+++ Assets/Scripts/Battle/Skills/TypesSkills/targetThisCharacter.cs
@@ -5,41 +5,9 @@ using System.Collections.Generic;
 [CreateAssetMenu(menuName = "TargetThisCharacter")]
 public class TargetThisCharacter : TypeSkill
 {
-    public GameObject ThisCharacter;
-    public string FallbackName;
-    public int FallbackIndex;
-
     public override IEnumerator Targets() {
-        CharactersAttributes = BuildListFrom(GetTarget());
+        CharactersAttributes = new List<CharacterAttributes>();
+        CharacterAttributes.Add(PlayerCharactersSkills.CharacterAttr);
         yield return null;
-    }
-
-    public void SetThisCharacter(GameObject go)  {
-        ThisCharacter = go; 
-    }
-
-    public void ClearThisCharacter()  {
-        ThisCharacter = null; 
-    }
-
-    private GameObject[] GetAllChars() => GameObject.FindGameObjectsWithTag("Character");
-
-    private GameObject GetTarget() {
-        if (ThisCharacter != null) return ThisCharacter;
-        var chars = GetAllChars();
-        if (!string.IsNullOrEmpty(FallbackName)) {
-            for (int i = 0; i < chars.Length; i++)
-                if (chars[i].name == FallbackName) return chars[i];
-        }
-        if (chars.Length == 0) return null;
-        return chars[Mathf.Clamp(FallbackIndex, 0, chars.Length - 1)];
-    }
-
-    private List<CharacterAttributes> BuildListFrom(GameObject go) {
-        List<CharacterAttributes> list = new List<CharacterAttributes>();
-        if (go == null) return list;
-        var a = go.GetComponent<CharacterAttributes>();
-        if (a != null) list.Add(a);
-        return list;
     }
 }
