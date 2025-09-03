c7a97499a9f77c2d6b548b6dfe51b8af4ce1446b
Nicholas Nabity <nichonabity@gmail.com>
2025-09-03 14:46:34 -0300

    Reset Script

--- Assets/Scripts/UI/ResourceSystem/ManaSystem/ManaSystem.cs
+++ Assets/Scripts/UI/ResourceSystem/ManaSystem/ManaSystem.cs
@@ -3,14 +3,8 @@ using UnityEngine;
 public class ManaSystem : MonoBehaviour
 {
     static public ResourceSystem Mp;
-    private int level = 0;
     
     public void Constructor(ResourceSystem mp) {
         Mp = mp;
-    }
-
-    public void LevelUp() {
-        level += 1;
-        Mp = 10 + level;
     }
 }
