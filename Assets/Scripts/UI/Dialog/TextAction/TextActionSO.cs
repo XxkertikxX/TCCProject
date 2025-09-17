6c385139690c6fcd8bb60115887bbe2dc9ca1ff3
Nicholas Nabity <nichonabity@gmail.com>
2025-09-17 15:42:39 -0300

    TextAction

--- Assets/Scripts/UI/Dialog/TextAction/TextActionSO.cs
+++ Assets/Scripts/UI/Dialog/TextAction/TextActionSO.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "TextActionSO")]
 public class TextActionSO : TextActionString
 {
     [SerializeField] TextsSO texts;
