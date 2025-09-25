8b78a400680bfb3b17bd1825677ee9b1325763b0
gaelExp <pedroandmelo2000@gmail.com>
2025-09-25 11:02:45 -0300

    SecondBattle

--- Assets/Scripts/UI/ResourceSystem/HpSystem/Death/DestroyDeath.cs
+++ Assets/Scripts/UI/ResourceSystem/HpSystem/Death/DestroyDeath.cs
@@ -1,8 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class DestroyDeath : MonoBehaviour, IDeath
 {
     public void Death() {
+        StartCoroutine(DeathAnim());
+    }
+
+    public IEnumerator DeathAnim()
+    {
+        EnemyAnim.PlayTrigger("Died");
+        yield return new WaitForSeconds(3f);
         Destroy(gameObject);
     }
 }
