130eef92c11e7751b39b3c940153c50453429251
Nicholas Nabity <nichonabity@gmail.com>
2025-10-07 17:06:18 -0300

    Refactoring

--- Assets/Scripts/Audios/GameAudioManager.cs
+++ Assets/Scripts/Audios/GameAudioManager.cs
@@ -7,43 +7,36 @@ public class GameAudioManager : MonoBehaviour
     [SerializeField] private SoundList[] soundsList;
     private static GameAudioManager instance;
     private AudioSource audioSource;
-    private void Awake()
-    {
+
+    private void Awake() {
         instance = this;
     }
 
-    private void Start()
-    {
+    private void Start() {
         audioSource = GetComponent<AudioSource>();
     }
 
-    public static void PlaySound(SoundTypes audioClips)
-    {
+    public static void PlaySound(SoundTypes audioClips) {
         AudioClip[] clips = instance.soundsList[(int)audioClips].Sounds;
         AudioClip randomClip = clips[UnityEngine.Random.Range(0,clips.Length)];
         if(audioClips != SoundTypes.Music)
             instance.audioSource.PlayOneShot(randomClip, getTypeOfVolume(audioClips));
     }
 
-    private static float getTypeOfVolume(SoundTypes s)
-    {
-        if(s != SoundTypes.Music)
-        {
+    private static float getTypeOfVolume(SoundTypes s) {
+        if(s != SoundTypes.Music) {
             return SliderEffect.volumeGeneral;
         }
-        else
-        {
+        else {
             return SliderEffect.volumeMusic;
         }
     }
 
 #if UNITY_EDITOR
-    private void OnEnable()
-    {
+    private void OnEnable() {
         string[] names = Enum.GetNames(typeof(SoundTypes));
         Array.Resize(ref soundsList, names.Length);
-        for (int i = 0; i < soundsList.Length; i++)
-        {
+        for (int i = 0; i < soundsList.Length; i++) {
             soundsList[i].name = names[i];
         }
     }
@@ -51,8 +44,7 @@ public class GameAudioManager : MonoBehaviour
 #endif
 }
 [Serializable]
-public struct SoundList
-{
+public struct SoundList {
     public AudioClip[] Sounds { get => sounds; }
     [HideInInspector] public string name;
     [SerializeField] private AudioClip[] sounds;
