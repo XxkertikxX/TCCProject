0c94ebba512ca921f114ced93cb15344ae81fe6b
XxkertikxX <nichonabity@gmail.com>
2025-10-14 17:44:40 -0300

    Revert "VSF"
    This reverts commit 870e548050fdead07ac6c20251e483009e1ed670.

--- Assets/Scripts/Db Scripts/Save/SaveSystem.cs
+++ Assets/Scripts/Db Scripts/Save/SaveSystem.cs
@@ -4,8 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class SaveSystem
 {
-    public void Save() {
-        using (var db = new LiteDatabase(Path())) {
+    public void Save()
+    {
+        using (var db = new LiteDatabase(Path()))
+        {
             var col = db.GetCollection<SaveStats>("save_stats");
             var save = Load(db);
             save = Update(save);
@@ -13,8 +15,10 @@ public class SaveSystem
         }
     }
 
-    public void SaveBattle(int index, bool win) {
-        using (var db = new LiteDatabase(Path())) {
+    public void SaveBattle(int index, bool win)
+    {
+        using (var db = new LiteDatabase(Path()))
+        {
             var col = db.GetCollection<SaveStats>("save_stats");
             var save = Load(db);
             save = UpdateBattles(save, index, win);
@@ -22,24 +26,31 @@ public class SaveSystem
         }
     }
 
-    public SaveStats OpenLoad() {
-        using (var db = new LiteDatabase(Path())) {
+    public SaveStats OpenLoad()
+    {
+        using (var db = new LiteDatabase(Path()))
+        {
             return Load(db);
         }
     }
 
-    private SaveStats Load(LiteDatabase db) {
+    private SaveStats Load(LiteDatabase db)
+    {
         var col = db.GetCollection<SaveStats>("save_stats");
         var save = col.FindOne(Query.EQ("_id", 1));
-        if (save == null) {
+        if (save == null)
+        {
+            Debug.Log("Nenhum save encontrado, criando novo...");
             save = CreateNew();
             col.Upsert(save);
         }
         return save;
     }
 
-    private SaveStats CreateNew() {
-        var save = new SaveStats {
+    private SaveStats CreateNew()
+    {
+        var save = new SaveStats
+        {
             ID = 1,
             SceneName = "Tutorial1",
             ManaBase = 10,
@@ -51,7 +62,8 @@ public class SaveSystem
         return save;
     }
 
-    private SaveStats Update(SaveStats save) {
+    private SaveStats Update(SaveStats save)
+    {
         Transform player = GameObject.FindGameObjectWithTag("Player").transform;
 
         save.SceneName = SceneManager.GetActiveScene().name;
@@ -63,16 +75,19 @@ public class SaveSystem
         return save;
     }
 
-    private SaveStats UpdateBattles(SaveStats save, int index, bool win) {
-        save.DefeatEnemy[index] = win;
+    private SaveStats UpdateBattles(SaveStats save, int index, bool win)
+    {
+        save.DefeatEnemy[index] = !win;
         return save;
     }
 
-    private Vector3Stat NewVector3(float x, float y, float z) {
+    private Vector3Stat NewVector3(float x, float y, float z)
+    {
         return new Vector3Stat { X = x, Y = y, Z = z };
     }
 
-    private string Path() {
+    private string Path()
+    {
         return Application.persistentDataPath + "/save_stats.db";
     }
 }
