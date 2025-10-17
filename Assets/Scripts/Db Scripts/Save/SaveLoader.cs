106524ea2888e4ca6e35b45b5ed2329e3862a065
XxkertikxX <nichonabity@gmail.com>
2025-10-16 22:33:05 -0300

    SceneChange

--- Assets/Scripts/Db Scripts/Save/SaveLoader.cs
+++ Assets/Scripts/Db Scripts/Save/SaveLoader.cs
@@ -12,7 +12,8 @@ public class SaveLoader : MonoBehaviour
 
     private IEnumerator LoadSave(SaveSystem saveSystem) {
         SaveStats saveStats = saveSystem.OpenLoad();
-        yield return SceneManager.LoadSceneAsync(saveStats.SceneName);
+        yield return SceneManager.LoadSceneAsync(saveStats.SceneName, LoadSceneMode.Single);
+		Resources.UnloadUnusedAssets();
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         player.transform.position = new Vector3(saveStats.Player.X, saveStats.Player.Y, saveStats.Player.Z);
         LevelSystem.Level = saveStats.Level;
