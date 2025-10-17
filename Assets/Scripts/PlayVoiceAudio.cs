4f0862a12cc002a591ec85c6ca1c71ee93d312c5
gaelExp <pedroandmelo2000@gmail.com>
2025-10-17 17:22:52 -0300

    Eles falam

---
+++ Assets/Scripts/PlayVoiceAudio.cs
@@ -0,0 +1,37 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class PlayVoiceAudio : MonoBehaviour
+{
+    [SerializeField] private Text actualCharacter;
+    public void PlayVoice()
+    {
+        Debug.Log("Entrou");
+        GameAudioManager.PlaySound(CharacterTalking());
+    }
+
+    private SoundTypes CharacterTalking()
+    {
+        switch (actualCharacter.text)
+        {
+            case "Liora":
+                return SoundTypes.LioraTalking;
+            case "Alon":
+                return SoundTypes.AlonTalking;
+            case "Amber":
+                return SoundTypes.AmberTalking;
+            case "Clementime":
+                return SoundTypes.ClementimeTalking;
+            case "Rei":
+                break;
+            case "Fang":
+                break;
+            case "NPC2":
+                break;
+            case "NPC3":
+                break;
+        }
+        return SoundTypes.LioraTalking;
+    }
+}
