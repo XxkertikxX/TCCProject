bff1f627917965ed9de0845a580e304378842f96
unknown <aluno@p0610.etecaf.local>
2025-10-21 15:56:56 -0300

    Refactoring

--- Assets/Scripts/CinematicPlayer.cs
+++ Assets/Scripts/CinematicPlayer.cs
@@ -5,37 +5,34 @@ public class CinematicPlayer : MonoBehaviour
 {
     [SerializeField] private PlayableDirector director;
     private PlayerMovementSystem playerMove;
-    void Start()
-    {
+
+    void Start() {
         playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovementSystem>();
         director = GetComponent<PlayableDirector>();
     }
-    private void OnEnable()
-    {
+
+    private void OnEnable() {
         director.played += StartCinematic;
         director.stopped += EndCinematic;
     }
-    private void OnDisable()
-    {
+
+    private void OnDisable() {
         director.played -= StartCinematic;
         director.stopped -= EndCinematic;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
             director.Play(director.playableAsset);
         }
     }
 
-    private void StartCinematic(PlayableDirector aDirector)
-    {
+    private void StartCinematic(PlayableDirector aDirector) {
         GetComponent<Collider2D>().enabled = false;
         playerMove.enabled = false;
     }
-    private void EndCinematic(PlayableDirector aDirector)
-    {
+
+    private void EndCinematic(PlayableDirector aDirector) {
         playerMove.enabled = true;
     }
 }
