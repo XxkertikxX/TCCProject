5c8fead8f94feb8aa1da15c91b3b5116898ccafa
Pedro <nichonabity@gmail.com>
2025-10-22 16:55:42 -0300

    Battle

--- Assets/Scripts/Battle/AttackRhythm/NoteRhythm.cs
+++ Assets/Scripts/Battle/AttackRhythm/NoteRhythm.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
-public class NoteRhythm : AttackRhythm, IUpdateRhythm
-{   
+public class NoteRhythm : AttackRhythm, IUpdateRhythm 
+{
     private RhythmProperties rhythmProperties;
 
     private int totalLines;
     private int timesForInvoke = 1;
 
     private List<Queue<NoteMovement>> notes = new List<Queue<NoteMovement>>();
-    
+
     void Awake() {
         rhythmProperties = GetComponent<RhythmProperties>();
     }
-    
+
     public void UpdateAttack() {
         Click();
         DequeueLineOutLimits();
@@ -24,8 +25,8 @@ public class NoteRhythm : AttackRhythm, IUpdateRhythm
     }
 
     public void FixedUpdateAttack() {
-        foreach(Queue<NoteMovement> queue in notes){
-            foreach(NoteMovement note in queue) {
+        foreach (Queue<NoteMovement> queue in notes) {
+            foreach (NoteMovement note in queue) {
                 Directions direction = note.Direction;
                 Vector2 directionV2 = direction.Direction;
                 note.Rb.Move(directionV2, direction.Speed);
@@ -34,20 +35,20 @@ public class NoteRhythm : AttackRhythm, IUpdateRhythm
     }
 
     public override IEnumerator Attack(SkillBase skill) {
-        for(int i = 0; i < rhythmProperties.Index(); i++) {
+        for (int i = 0; i < rhythmProperties.Index(); i++) {
             notes.Add(new Queue<NoteMovement>());
         }
-        
+
         ActiveRhythm(true);
 
         totalLines = skill.TimesForInvoke;
         timesForInvoke = totalLines;
-        
+
         while (timesForInvoke > 0) {
             yield return StartCoroutine(InvokeLine(skill));
         }
 
-        while(!WithoutNotes()) {
+        while (!WithoutNotes()) {
             yield return null;
         }
     }
@@ -73,13 +74,43 @@ public class NoteRhythm : AttackRhythm, IUpdateRhythm
         noteMovement.Note = note;
         notes[direction.Index].Enqueue(noteMovement);
     }
-    
+
     private void Click() {
-        if(CheckInput(FirstNote().Peek())) {
+        List<string> directions = new List<string>();
+        foreach (Queue<NoteMovement> queue in notes) {
+            if (queue.Count > 0) {
+                directions.AddRange(queue.Peek().Note.Input);
+            }
+        }
+
+        bool equalsInput = directions.Count != directions.Distinct().Count();
+
+        if (!equalsInput) {
+            foreach (Queue<NoteMovement> queue in notes) {
+                if (queue.Count > 0) {
+                    CheckAndDequeueLine(queue.Peek());
+                }
+            }
+            return;
+        }
+
+        CheckAndDequeueLine(FirstNote().Peek());
+    }
+
+    private void CheckAndDequeueLine(NoteMovement firstNote) {
+        if (CheckInput(firstNote)) {
             DequeueLine(FirstNote());
         }
     }
 
+    private bool CheckInput(NoteMovement firstNote) {
+        foreach (string key in firstNote.Note.Input) {
+            if (InputCatalyst.input.InputButtonDown(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
     private void DequeueLine(Queue<NoteMovement> queue) {
         var note = queue.Dequeue();
         Damage += note.PerDamage(2) / totalLines;
@@ -87,9 +118,9 @@ public class NoteRhythm : AttackRhythm, IUpdateRhythm
     }
 
     private void DequeueLineOutLimits() {
-        foreach(Queue<NoteMovement> queue in notes) {
-            if(queue.Count == 0) continue;
-            if(queue.Peek().DestroyLineOutLimits()) {
+        foreach (Queue<NoteMovement> queue in notes) {
+            if (queue.Count == 0) continue;
+            if (queue.Peek().DestroyLineOutLimits()) {
                 var note = queue.Dequeue();
                 Destroy(note.gameObject);
             }
@@ -97,9 +128,9 @@ public class NoteRhythm : AttackRhythm, IUpdateRhythm
     }
 
     private void CloseRhythm() {
-        if(timesForInvoke != 0) return;
-        foreach(Queue<NoteMovement> queue in notes) {
-            if(queue.Count != 0) return;
+        if (timesForInvoke != 0) return;
+        foreach (Queue<NoteMovement> queue in notes) {
+            if (queue.Count != 0) return;
         }
         ActiveRhythm(false);
     }
@@ -113,17 +144,8 @@ public class NoteRhythm : AttackRhythm, IUpdateRhythm
         return true;
     }
 
-    private bool CheckInput(NoteMovement firstNote) {
-        foreach(string key in firstNote.Note.Input) {
-            if(InputCatalyst.input.InputButtonDown(key)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     private void ChangeSprites() {
-        foreach(Queue<NoteMovement> queue in notes){
+        foreach (Queue<NoteMovement> queue in notes) {
             Directions dir = queue.Peek().Direction;
             Notes notes = queue.Peek().Note;
             dir.SprRendCenterLine.sprite = notes.CenterLineSpr;
@@ -134,25 +156,23 @@ public class NoteRhythm : AttackRhythm, IUpdateRhythm
         Queue<NoteMovement>[] queues = notes.ToArray();
         Queue<NoteMovement> queue = new Queue<NoteMovement>();
 
-        if(queues.Length == 1) {
-            return queues[0];
-        }
+        if (queues.Length == 1) return queues[0];
 
-        for(int i = 0; i < queues.Length - 1; i++) {
-            if(queues[i].Count == 0 && queues[i+1].Count == 0) continue;
-            if(queues[i+1].Count == 0 || queues[i].Count == 0) {
-                if(queues[i+1].Count == 0) {
+        for (int i = 0; i < queues.Length - 1; i++) {
+            if (queues[i].Count == 0 && queues[i + 1].Count == 0) continue;
+            if (queues[i + 1].Count == 0 || queues[i].Count == 0) {
+                if (queues[i + 1].Count == 0) {
                     queue = queues[i];
                 } else {
-                    queue = queues[i+1];
+                    queue = queues[i + 1];
                 }
                 continue;
             }
 
-            if(DistanceToCenter(queues[i+1].Peek()) > DistanceToCenter(queues[i].Peek())) {
-                queue = queues[i+1];
-            } else { 
-                queue = queues[i]; 
+            if (DistanceToCenter(queues[i + 1].Peek()) > DistanceToCenter(queues[i].Peek())) {
+                queue = queues[i + 1];
+            } else {
+                queue = queues[i];
             }
         }
         return queue;
