b11113fa064b65db5dbdba0605b994ee6d992f08
XxkertikxX <nichonabity@gmail.com>
2025-11-19 17:01:05 -0300

    Implements Icons

--- Assets/Scripts/Battle/Skills/ApplySkillUI.cs
+++ Assets/Scripts/Battle/Skills/ApplySkillUI.cs
@@ -5,6 +5,7 @@ public class ApplySkillUI : MonoBehaviour
 {
 	[SerializeField] private int position;
 	[SerializeField] private SpriteRenderer iconTypeSkill;
+	[SerializeField] private SpriteRenderer[] iconSubTypeSkill;
 	[SerializeField] private Text damage;
 	[SerializeField] private Text manaConsume;
 	
@@ -12,8 +13,15 @@ public class ApplySkillUI : MonoBehaviour
 		iconTypeSkill.sprite = Skill().SpriteTypeSkill;
 		damage.text = Skill().SkillPower.ToString();
 		manaConsume.text = Skill().ManaConsume.ToString();
+		SubType();
 	}
 	
+	private void SubType() {
+		for(int i = 0; i < Skill().SpritesEffectsSkill.Length; i++) {
+			iconSubTypeSkill[i].sprite = Skill().SpritesEffectsSkill[i];
+		}
+	}
+
 	private SkillBase Skill() {
         return CharacterClick.CharacterAttr.Character.Skills[position];
     }
