917f0233d1d41eb1cfc7f221d62144fdd5e6de6f
XxkertikxX <nichonabity@gmail.com>
2025-11-20 14:29:31 -0300

    Sei lá

--- Assets/Scripts/UI/Dialog/DialogWriter/DialogWriterGeneric.cs
+++ Assets/Scripts/UI/Dialog/DialogWriter/DialogWriterGeneric.cs
@@ -96,7 +96,8 @@ public class DialogWriterGeneric : MonoBehaviour, IDialogWriter
     {
         if (voice == null)
             voice = GetComponent<PlayVoiceAudio>();
-        //voice.PlayVoice();
+		if(voice != null)
+			voice.PlayVoice();
     }
 
 
