20eda10bcb628a21795766967b7f4d0028ec2a7b
XxkertikxX <nichonabity@gmail.com>
2025-11-20 17:28:46 -0300

    Change folders

---
+++ Assets/Scripts/Upgrades/RandomUpgrade.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomUpgrade : MonoBehaviour
+{
+    static public UpgradeBase RandomUpgradeSelect() {
+		
+	}
+}
