c3147e29334ef3aee4157d2b21d0548b4e1aa2f9
XxkertikxX <nichonabity@gmail.com>
2025-11-20 14:09:58 -0300

    Tutorial

---
+++ Assets/Scripts/UI/Dialog/DialogSetup/DialogTutorial.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTutorial : MonoBehaviour, IDialogSetup
+{
+	[SerializeField] private GameObject tutorialGO;
+    [SerializeField] private string[] bindsPressed;
+	
+    public void SetupOpenDialog() {}
+    
+    public void SetupCloseDialog(){
+		StartCoroutine(BattleTutorial());
+    }
+	
+	private IEnumerator BattleTutorial() {
+		tutorialGO.SetActive(true);
+		yield return new WaitForKeyDown(bindsPressed);
+		tutorialGO.SetActive(false);
+	}
+	
+	
+}
+
+public class WaitForKeyDown : CustomYieldInstruction {
+	private string[] binds;
+
+    public WaitForKeyDown(string[] binds) {
+        this.binds = binds;
+    }
+	
+    public override bool keepWaiting {
+        get {
+			foreach (string bind in binds) {
+				if (InputCatalyst.input.InputButtonDown(bind)) {
+					return false;
+				}
+			}
+            return true;
+        }
+    }
+}
