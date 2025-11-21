957e10cb01655b0f147cb4e02ee9c1d6e52cb488
XxkertikxX <nichonabity@gmail.com>
2025-11-21 14:40:45 -0300

    UpdateNameText

---
+++ Assets/Scripts/Audios/AtualCharacterText.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtualCharacterText : MonoBehaviour
+{
+    [SerializeField] private Text name;
+	
+	void Update() {
+		if(CharacterClick.CharacterAttr != null) {
+			name.text = CharacterClick.CharacterAttr.Character.Name;
+		}
+	}
+} 
