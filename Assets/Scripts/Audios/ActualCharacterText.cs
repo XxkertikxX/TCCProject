362650e136461bd2f7942054e3101e5eebe68e83
XxkertikxX <nichonabity@gmail.com>
2025-11-21 17:36:00 -0300

    Balanceamento

--- Assets/Scripts/Audios/ActualCharacterText.cs
+++ Assets/Scripts/Audios/ActualCharacterText.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AtualCharacterText : MonoBehaviour
+public class ActualCharacterText : MonoBehaviour
 {
     [SerializeField] private Text name;
 	
