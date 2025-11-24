40c0ea6add5f9ddc2a05ace1698d085a4801ab18
XxkertikxX <nichonabity@gmail.com>
2025-11-24 12:14:57 -0300

    Tirar debug

--- Assets/Scripts/applyiconcharacter.cs
+++ Assets/Scripts/applyiconcharacter.cs
@@ -6,7 +6,6 @@ public class applyiconcharacter : MonoBehaviour
 	[SerializeField] private Text name;
     void OnEnable() {
 		if(CharacterClick.CharacterAttr != null) {
-			Debug.Log(CharacterClick.CharacterAttr);
 			GetComponent<Image>().sprite = CharacterClick.CharacterAttr.Character.Icon;
 			name.text = CharacterClick.CharacterAttr.Character.Name;
 		}
