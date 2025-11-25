c05018561b295eb627f104b60c4b08d41fa55b11
gaelExp <pedroandmelo2000@gmail.com>
2025-11-25 17:25:07 -0300

    huh

--- Assets/Scripts/Db Scripts/Save/SaveTrigger.cs
+++ Assets/Scripts/Db Scripts/Save/SaveTrigger.cs
@@ -5,13 +5,15 @@ using System.Collections;
 public class SaveTrigger : MonoBehaviour
 {
     static public event Action OnDeath;
-    private bool morreu;
+    private bool morreu = false;
+    [SerializeField] private GameObject MorreuMenu;
 
     void OnTriggerEnter2D(Collider2D collision) {
         if(collision.CompareTag("Player") && !morreu) {
-            GameObject.FindObjectOfType<SaveLoader>().Load();
-            OnDeath?.Invoke();
-            StartCoroutine(Morreu());
+            //GameObject.FindObjectOfType<SaveLoader>().Load();
+            MorreuMenu.SetActive(true);
+            //OnDeath?.Invoke();
+            //StartCoroutine(Morreu());
         }
     }
 
