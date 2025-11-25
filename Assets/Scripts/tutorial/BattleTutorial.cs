c28771f36e52e6f54793c920a2157be65f556906
XxkertikxX <nichonabity@gmail.com>
2025-11-25 15:50:10 -0300

    aq

--- Assets/Scripts/tutorial/BattleTutorial.cs
+++ Assets/Scripts/tutorial/BattleTutorial.cs
@@ -1,8 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleTutorial : MonoBehaviour
 {
     public bool hasTutorial;
+    [SerializeField] private GameObject[] tutoriais;
+
+    private void OnEnable() {
+        CharacterAttack.OnCharacterPreparedAttack += ActiveTutorial;
+    }
+
+    private void OnDisable() {
+        CharacterAttack.OnCharacterPreparedAttack -= ActiveTutorial;
+    }
+
+    private void ActiveTutorial() {
+        if (hasTutorial) {
+            StartCoroutine(Active());
+        }
+    }
+
+    private IEnumerator Active() {
+        foreach(var tutorial in tutoriais) {
+            yield return null;
+        }
+    }
 }
