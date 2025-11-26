0b2de5f8a693e27f664a29f3eb019362f197795e
gaelExp <pedroandmelo2000@gmail.com>
2025-11-26 15:42:49 -0300

    Só botei de volta, tinha colocado de testar

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -7,44 +7,42 @@ public class MusicPlayer : MonoBehaviour
 {
     [SerializeField] private LevelsMusic[] musics;
     private MusicPlayer instance;
-    private AudioSource musicAudioSource;
+    [SerializeField] private AudioSource musicAudioSource;
 
     private void Awake() //fazer fadeIn e fadeOut no audio
     {
         instance = this;
-    }
-
-    private void Start()
-    {
         musicAudioSource = GetComponent<AudioSource>();
-        PlayMusic(SceneManager.GetActiveScene(), new Scene());
+        Debug.Log(musicAudioSource.isPlaying);
     }
 
     private void Update()
     {
         musicAudioSource.volume = GameAudioManager.musicVolume;
-
-        if (musicAudioSource.isPlaying)
-            return;
-
-        
     }
 
-    private void PlayMusic(Scene actual, Scene next)
+    private void PlayMusic(Scene actual, LoadSceneMode a)
     {
         for (int i = 0; i < musics.Length; i++)
         {
             for (int j = 0; j < musics[i].ScenesWithMusic.Length; j++)
             {
-                if (musics[i].ScenesWithMusic[j] == actual.name && !musicAudioSource.isPlaying)
+                if (musicAudioSource.isPlaying)
+                {
+                    Debug.Log("Cena tocando: " + musics[i].ScenesWithMusic[j] + "|| Musica tocando: " + musics[i].MusicClip.name);
+                    //return;
+                }
+                
+                if (musics[i].ScenesWithMusic[j] == SceneManager.GetActiveScene().name)
                 {
                     instance.musicAudioSource.clip = musics[i].MusicClip;
                     instance.musicAudioSource.Play();
-                    Debug.Log("Cena tocando: " + musics[i].ScenesWithMusic[j] + "|| Musica tocando: " + musics[i].MusicClip.name);
+                    return;
                 }
-                if (actual.name != musics[i].ScenesWithMusic[j] && musicAudioSource.isPlaying)
+                else
                 {
-                    instance.musicAudioSource.Stop();
+                    musicAudioSource.Stop();
+                    //PlayMusic(SceneManager.GetActiveScene(), new LoadSceneMode());
                 }
             }
         }
@@ -52,12 +50,13 @@ public class MusicPlayer : MonoBehaviour
 
     private void OnEnable()
     {
-        SceneManager.activeSceneChanged += PlayMusic;
+        SceneManager.sceneLoaded += PlayMusic;
+        
     }
 
     private void OnDisable()
     {
-        SceneManager.activeSceneChanged -= PlayMusic;
+        SceneManager.sceneLoaded -= PlayMusic;
     }
 }
 
