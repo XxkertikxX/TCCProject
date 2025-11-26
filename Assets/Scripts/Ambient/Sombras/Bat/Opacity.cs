948105a8e792d554627799ef256817967442b724
bryan270906 <bjesusdealbuquerque@gmail.com>
2025-11-26 19:17:00 -0300

    AudioCoraçãoMorcego

--- Assets/Scripts/Ambient/Sombras/Bat/Opacity.cs
+++ Assets/Scripts/Ambient/Sombras/Bat/Opacity.cs
@@ -1,47 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class Opacity : MonoBehaviour
 {
-    public float minAlpha = 50f / 255f;  
-    public float maxAlpha = 140f / 255f;  
-    public float speed = 1f;             
+    [SerializeField, Range(0f, 1f)]
+    private float minAlpha = 50f / 255f;
+    [SerializeField, Range(0f, 1f)]
+    private float maxAlpha = 140f / 255f;
+    [SerializeField, Min(0f)]
+    private float speed = 1f;
 
     private Image img;
     private bool goingUp = true;
 
-    void Start()
+    void Awake()
     {
         img = GetComponent<Image>();
+        if (img != null)
+        {
+            Color c = img.color;
+            c.a = Mathf.Clamp(c.a, minAlpha, maxAlpha);
+            img.color = c;
+        }
+    }
+
+    void OnValidate()
+    {
+        minAlpha = Mathf.Clamp01(minAlpha);
+        maxAlpha = Mathf.Clamp01(maxAlpha);
+        if (minAlpha > maxAlpha) minAlpha = maxAlpha;
+        if (speed < 0f) speed = 0f;
     }
 
     void Update()
     {
         if (img == null) return;
 
-        Color cor = img.color;
+        Color c = img.color;
+        float target = goingUp ? maxAlpha : minAlpha;
+        c.a = Mathf.MoveTowards(c.a, target, speed * Time.deltaTime);
 
-        if (goingUp)
+        if (Mathf.Approximately(c.a, target))
         {
-            cor.a += speed * Time.deltaTime;
-            if (cor.a >= maxAlpha)
+            if (goingUp && c.a >= maxAlpha) goingUp = false;
+            else if (!goingUp && c.a <= minAlpha)
             {
-                cor.a = maxAlpha;
-                goingUp = false;
-            }
-        }
-        else
-        {
-            cor.a -= speed * Time.deltaTime;
-            if (cor.a <= minAlpha)
-            {
-                cor.a = minAlpha;
                 goingUp = true;
+                GameAudioManager.PlaySound(SoundTypes.NPC3Talking);
             }
         }
 
-        img.color = cor;
+        img.color = c;
     }
 }
