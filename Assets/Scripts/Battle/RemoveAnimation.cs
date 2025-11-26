29d27c0e56ae6090a2c977902fb7c60e3a9ce06d
XxkertikxX <103328703+XxkertikxX@users.noreply.github.com>
2025-11-26 16:36:11 -0300

    l

--- Assets/Scripts/Battle/RemoveAnimation.cs
+++ Assets/Scripts/Battle/RemoveAnimation.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using System.Collections;
 
 public class RemoveAnimation : MonoBehaviour
 {
     void OnDisable() {
-        if (CharacterClick.CharacterAttr != null) { 
-            GetComponent<Animator>().enabled = false;
+        if (CharacterClick.CharacterAttr != null) {
+            StartCoroutine(dis());
         } 
+    }
+
+    private IEnumerator dis() {
+        yield return new WaitForSeconds(1.5f);
+        GetComponent<Animator>().enabled = false;
     }
 }
